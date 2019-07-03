47fbbd8ead3586888273909eeeff23f763ec028a
Jose Quispe <jose.quispe@avantica.net>
2019-07-03 16:19:51 -0500

    add cardview styles in api 19, 21 an above

---
+++ app/src/main/java/com/jflavio1/materialpoc/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jflavio1.materialpoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
